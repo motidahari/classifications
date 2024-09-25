6ba90d7e910e2d83b05abb2143f339e4d76f4e8d
Moti-Dahari <moti.d@morning.co>
2024-09-25 15:56:36 +0300

    Return expenses list

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,8 @@ const apiGatewayHandler = serviceHandler.enableApiGatewayHandler();
 apiGatewayHandler.app.get('/', async (req, res) => {
   try {
     const data = await appContainer.expensesService.getExpenses();
-    // console.log('data', data[0]);
 
-    res.send(data[0]);
+    res.send(data);
   } catch (error) {
     console.log('error', error);
   }
